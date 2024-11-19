186f0e0d9da143945e93a36aec952515ecaa56c8
Epistaxiophobic <xerotheprogrammer@gmail.com>
2024-11-19 15:28:03 +0330

    update and debug tokenization functionality

--- jsparser.py
+++ jsparser.py
@@ -1,4 +1,4 @@
-def tokenize(s):
+def _tokenize(s):
     """
     Returns a list of tokens from a give string
     """
@@ -9,10 +9,13 @@ def tokenize(s):
         if c in symbols:
             if len(curr_tkn) > 0:
                 token_lst.append(curr_tkn)
+                curr_tkn = ""
             token_lst.append(c)
         elif c.isspace():
             if len(curr_tkn) > 0:
                 token_lst.append(curr_tkn)
+                curr_tkn = ""
         else:
             curr_tkn += c
+    print(token_lst)
     return token_lst
